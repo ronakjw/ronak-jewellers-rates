9287a5d31f764dc9384c0f1fbbe8e530265713b0
ronakjw <rrmctexim@gmail.com>
2026-05-28 15:09:55 +0530

    Simplify root layout

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,7 @@
-export const metadata = {
-  title: "Ronak Jewellers",
-  description: "Live MCX Silver Rates"
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          background: "#0d0d0d"
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
-
