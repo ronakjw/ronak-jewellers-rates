c54dfb61709b43e59c6dd6381918313a306bd0e3
ronakjw <rrmctexim@gmail.com>
2026-07-31 14:36:18 +0530

    Update index.js

--- app/lib/providers/index.js
+++ app/lib/providers/index.js
@@ -1,1 +1,16 @@
+import * as kite from "./kite";
+import * as fivepaisa from "./fivepaisa";
 
+const provider =
+  process.env.MARKET_PROVIDER?.toLowerCase() || "kite";
+
+export function getMarketProvider() {
+  switch (provider) {
+    case "fivepaisa":
+      return fivepaisa;
+
+    case "kite":
+    default:
+      return kite;
+  }
+}
