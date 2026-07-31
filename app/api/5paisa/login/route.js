cb3f5eba25bc65228bde9847d466fcf2b2d20d38
ronakjw <rrmctexim@gmail.com>
2026-07-31 17:22:03 +0530

    Delete app/api/5paisa directory

--- app/api/5paisa/login/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  const vendorKey = process.env.FIVEPAISA_API_KEY;
-  const responseUrl = encodeURIComponent(process.env.FIVEPAISA_REDIRECT_URL);
-
-  const url =
-    `https://dev-openapi.5paisa.com/WebVendorLogin/VLogin/Index?VendorKey=${vendorKey}&ResponseURL=${responseUrl}`;
-
-  return Response.redirect(url);
-}
