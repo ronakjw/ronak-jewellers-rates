b785b1966fd454362160041ed38b952b7d3a4bd7
ronakjw <rrmctexim@gmail.com>
2026-07-31 15:09:47 +0530

    Update route.js

--- app/api/kite-quote/route.js
+++ app/api/kite-quote/route.js
@@ -2,6 +2,7 @@ import { adminDb } from "../../../lib/firebaseAdmin";
 import {toBool, cleanSymbol, getBestPrices, getIndiaDateString,} from "../../../lib/market/utils";
 import { getSettings } from "../../../lib/market/settings";
 import { getActiveContractRow, getContractRowBySymbol,} from "../../../lib/market/contracts";
+import { getMcxRows, parseCsvLine,} from "../../../lib/market/instruments";
 
 export const dynamic = "force-dynamic";
 
